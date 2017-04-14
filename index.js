b3ab6f678bbdddecf24ee0141065ab4fb5a3fe30
Serena <serena.queen@hotmail.com>
2017-04-14 14:54:03 +0800

    modification

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var modal = new Modal();
-modal.show();
-modal.hide();
+$(function () {
+    // $(document).on('click', '.modal-demo-btn', function (e) {
+    //     var $this = $(e.target);
+    //     var $target  = $this.attr('data-target');
+    // });
+
+    $('.modal-demo-btn').on('click', function () {
+        var mymodal = $('#mymodal');
+        var op = mymodal.css('opacity');
+        if (op == 0) {
+            mymodal.css('opacity', 1);
+            mymodal.addClass('mymodal-transition');
+        } else {
+            mymodal.css('opacity', 0);
+        }
+    });
+
+    $('.close-btn,.save-btn,.remove-circle, #modal-cover').on('click', function () {
+        var mymodal = $('#target');
+        var op = mymodal.css('opacity');
+        if (op == 1) mymodal.css('opacity', 0);
+    });
+
+});
